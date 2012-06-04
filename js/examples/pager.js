ee8bfdee6bc942187cb3d91860193c555dbf36ef
Michael Hellein <michael@bearded.com>
2012-06-04 09:08:13 -0400

    Site updated at 2012-06-04 13:08:13 UTC

---
+++ js/examples/pager.js
@@ -0,0 +1,21 @@
+$(function(){
+  var $pager = $('.cavendish-pager');
+
+  $('.cavendish')
+    // This allows us to set up a pager, if we feel so inclined.
+    .bind('cavendish-slide-init', function(e, index, slide, cavendish) {
+      $pager.find('a').eq(index)
+        .click(function(){
+          cavendish.goto(index)
+          return false
+        })
+    })
+    .cavendish()
+    .bind('cavendish-transition', function(e, c) {
+      $pager.find('li')
+        .removeClass('active')
+        .eq(c.index)
+        .addClass('active');
+    })
+
+})
