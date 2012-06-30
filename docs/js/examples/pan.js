523392db75a9fb4364bf61e0ca6d2b07507e04b3
Michael Hellein <michael@bearded.com>
2012-06-29 22:59:14 -0400

    refined examples

--- docs/js/examples/pan.js
+++ docs/js/examples/pan.js
@@ -1,14 +1,32 @@
 $(function(){
   var $pager = $('.cavendish-pager');
+  var $background = $('.cavendish-background ol.slides');
 
   $('.cavendish')
     .bind('cavendish-slide-init', function(e, index, slide, cavendish) {
-      slide.css('left', index*100+'%')
+
+      // Duplicated from pager.js.
+      $pager.find('a').eq(index)
+        .click(function(){
+          cavendish.goto(index)
+          return false
+        })
+
+      $background.children().eq(index).css('left', index*100+'%')
     })
-    .cavendish()
+    .cavendish({
+      slideSelector: '.cavendish-navigation > ol > li'
+    })
     .bind('cavendish-transition', function(e, c) {
+
+      // Duplicated from pager.js.
+      $pager.find('li')
+        .removeClass('active')
+        .eq(c.index)
+        .addClass('active');
+
       var left = c.index * -100;
-      $(this).children().css('left', left+'%');
+      $background.css('left', left+'%');
     });
 
 })
