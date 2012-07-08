729d258034a90137a3f89409059ecdc2a70d03e4
Michael Hellein <michael@bearded.com>
2012-07-07 21:35:51 -0400

    Site updated at 2012-07-08 01:35:51 UTC

--- js/examples/pan.js
+++ js/examples/pan.js
@@ -1,32 +1,8 @@
 $(function(){
-  var $pager = $('.cavendish-pager');
-  var $background = $('.cavendish-background ol.slides');
 
-  $('.cavendish')
-    .bind('cavendish-slide-init', function(e, index, slide, cavendish) {
-
-      // Duplicated from pager.js.
-      $pager.find('a').eq(index)
-        .click(function(){
-          cavendish.goto(index)
-          return false
-        })
-
-      $background.children().eq(index).css('left', index*100+'%')
-    })
-    .cavendish({
-      slideSelector: '.cavendish-navigation > ol > li'
-    })
-    .bind('cavendish-transition', function(e, c) {
-
-      // Duplicated from pager.js.
-      $pager.find('li')
-        .removeClass('active')
-        .eq(c.index)
-        .addClass('active');
-
-      var left = c.index * -100;
-      $background.css('left', left+'%');
-    });
+  $('.cavendish').cavendish({
+    slideSelector: '.cavendish-navigation > ol > li',
+    use_plugins: ['pager', 'pan']
+  });
 
 })
